26944da330696149894144b6dc33fb83686c0e63
Caio Castro <lcastro.caio@gmail.com>
2025-10-22 23:59:46 -0300

    autocomplete looking good

--- init.lua
+++ init.lua
@@ -8,13 +8,13 @@ vim.o.expandtab = true
 vim.o.termguicolors = true
 vim.cmd('colorscheme unokai')
 
--- KEYMAPS
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
--- Open init.lua
-vim.keymap.set('n', '<leader>rc', ':e $MYVIMRC<CR>', { desc = 'Open [R]C config' })
--- Open Lazy
-vim.keymap.set('n', '<leader>L', ':Lazy<CR>', { desc = 'Lazy.nvim UI' })
+-- KEYMAPS
+vim.keymap.set('n', '<leader>rc', ':e $MYVIMRC<CR>', { desc = 'Open [R]C config' }) -- Open init.lua
+vim.keymap.set('n', '<leader>L', ':Lazy<CR>', { desc = 'Lazy.nvim UI' }) -- Open Lazy
+vim.keymap.set('n', '<Esc>', ':nohlsearch<CR>', { desc = 'Clear search highlight' })
+vim.keymap.set('i', '<C-Space>', '<C-x><C-o>', { noremap = true, silent = true })
 
 -- LSP
 vim.lsp.config['luals'] = {
@@ -30,17 +30,34 @@ vim.lsp.config['luals'] = {
   }
 }
 vim.lsp.config['pyrefly'] = {
-  cmd = { 'pyrefly' },
+  cmd = { 'pyrefly' , 'lsp' },
   filetypes = { 'python' },
-  root_markers = { '.git' },
+  root_markers = { '.git', 'pyproject.toml', 'setup.py', 'requirements.txt' },
   settings = {
   },
   on_exit = function(code, _, _)
       vim.notify("Closing Pyrefly LSP exited with code: " .. code, vim.log.levels.INFO)
   end,
 }
-
 vim.lsp.enable({'luals', 'pyrefly'})
+-- activate completion
+-- Use CTRL-Y to select an item. |complete_CTRL-Y|
+vim.opt.completeopt = 'menuone,noselect,popup'
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = function(args)
+        local bufnr = args.buf
+        vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+        -- Help with signature help
+        vim.keymap.set('i', '<C-h>', vim.lsp.buf.signature_help, {buffer = bufnr})
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = bufnr, desc = "Hover documentation" })
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = bufnr, desc = "Go to definition" })
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, { buffer = bufnr, desc = "Find references" })
+        vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { buffer = bufnr, desc = "Code action" })
+    end,
+})
 
 -- DIAGNOSTICS
 vim.diagnostic.config({
@@ -51,67 +68,87 @@ vim.diagnostic.config({
     }
 })
 
-
 -- SETUP LAZY.NVIM
--- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
--- if not (vim.uv or vim.loop).fs_stat(lazypath) then
--- 	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
--- 	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
--- 	if vim.v.shell_error ~= 0 then
--- 		vim.api.nvim_echo({
--- 			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
--- 			{ out,                            "WarningMsg" },
--- 			{ "\nPress any key to exit..." },
--- 		}, true, {})
--- 		vim.fn.getchar()
--- 		os.exit(1)
--- 	end
--- end
--- vim.opt.rtp:prepend(lazypath)
--- require("lazy").setup({
--- 	spec = {
--- 		{
--- 			"folke/tokyonight.nvim",
--- 			lazy = false,
--- 			priority = 1000,
--- 			config = function()
--- 				vim.cmd([[colorscheme tokyonight]])
--- 			end,
--- 		},
--- 		-- {
--- 		-- 	"folke/trouble.nvim",
--- 		-- 	dependencies = { "nvim-tree/nvim-web-devicons" },
--- 		-- 	opts = {},
--- 		-- },
--- 		-- {
--- 		-- 	'sontungexpt/better-diagnostic-virtual-text',
--- 		-- 	config = function(_)
--- 		-- 		require('better-diagnostic-virtual-text').setup(opts)
--- 		-- 	end,
--- 		-- },
--- 		{
--- 			"nvim-treesitter/nvim-treesitter",
--- 			branch = "main",
--- 			lazy = false,
--- 			build = ":TSUpdate",
--- 		},
--- 		{
--- 			"neovim/nvim-lspconfig",
--- 		}
--- 	},
--- 	-- configure any other settings here. see documentation for detailes.
--- 	-- automatically check for plugin updates
--- 	checker = { enabled = true },
--- })
---
--- -- SETUP NVIM-TREESITTER
--- require 'nvim-treesitter'.install { "c", "lua", "markdown", "markdown_inline", "python", "zig", "superhtml", "wgsl", "zig" }
--- vim.api.nvim_create_autocmd('FileType', {
--- 	pattern = { '<filetype>' },
--- 	callback = function() vim.treesitter.start() end,
--- }) -- highlighting
--- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()' -- folds
--- vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()" -- indentation
---
--- -- SETUP LSP
--- vim.lsp.enable({'luals', })
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out,                            "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({
+	spec = {
+		{
+			"folke/tokyonight.nvim",
+			lazy = false,
+			priority = 1000,
+			config = function()
+				vim.cmd([[colorscheme tokyonight]])
+			end,
+		},
+		{
+			"nvim-treesitter/nvim-treesitter",
+			branch = "main",
+			lazy = false,
+			build = ":TSUpdate",
+		},
+    {
+          "hrsh7th/nvim-cmp",
+          dependencies = {
+            "hrsh7th/cmp-nvim-lsp", -- LSP completion source
+            "hrsh7th/cmp-buffer",   -- Buffer words completion
+            "hrsh7th/cmp-path",     -- File path completion
+            "L3MON4D3/LuaSnip",     -- Snippet engine (optional but recommended)
+          },
+          config = function()
+            local cmp = require("cmp")
+            cmp.setup({
+              snippet = {
+                expand = function(args)
+                  -- For LuaSnip (optional)
+                  require("luasnip").lsp_expand(args.body)
+                end,
+              },
+              mapping = cmp.mapping.preset.insert({
+                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<C-e>"] = cmp.mapping.abort(),
+                ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item
+                ["<Tab>"] = cmp.mapping.select_next_item(),
+                ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+              }),
+              sources = cmp.config.sources({
+                { name = "nvim_lsp" }, -- LSP completions
+                { name = "buffer" },   -- Current buffer words
+                { name = "path" },     -- File system paths
+              }),
+              -- Better completion experience
+              completion = {
+                completeopt = "menu,menuone,noinsert,noselect",
+              },
+            })
+          end,
+        },
+	},
+	-- configure any other settings here. see documentation for detailes.
+	-- automatically check for plugin updates
+	checker = { enabled = true },
+})
+
+-- SETUP NVIM-TREESITTER
+require 'nvim-treesitter'.install { "c", "lua", "markdown", "markdown_inline", "python", "zig", "superhtml", "wgsl", "zig" }
+vim.api.nvim_create_autocmd('FileType', {
+	pattern = { '<filetype>' },
+	callback = function() vim.treesitter.start() end,
+}) -- highlighting
+vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()' -- folds
+vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()" -- indentation
